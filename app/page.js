f5fe14ac9e70f64ff5df12fe220f94937791bffd
Kavi-Z <kavindusanga1331@gmail.com>
2025-07-19 00:14:32 +0530

    footer change3

--- app/page.js
+++ app/page.js
@@ -864,7 +864,7 @@ export default function HomePage() {
           }
  .footer-content {
           max-width: 1024px;
-          height: 20px;
+          height: 0px;
           margin: 0 auto;
           text-align: center;
           color: white;
