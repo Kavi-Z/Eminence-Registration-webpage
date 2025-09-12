02e03e78ceaf9fb1af49fcc19ef5b4e8ede241a9
Kavindu Sanjitha <kavindusanga1331@gmail.com>
2025-09-12 19:26:09 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -127,7 +127,7 @@ const toggleQuestion = (questionId) => {
       setIsLoading(false);
     }, 2000);
 
-    const targetDate = new Date("2025-09-30T09:00:00");
+    const targetDate = new Date("2025-09-20T09:00:00");
     const timer = setInterval(() => {
       const now = new Date();
       const distance = targetDate - now;
